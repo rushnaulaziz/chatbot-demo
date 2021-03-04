b37ed8af1ec3993a2cc4743bbe0377e0af1eb2ff
rushna nizami <rushnaulaziz@gmail.com>
2021-03-04 11:04:56 -0500

    Adds intents

--- chatbotkeras.py
+++ chatbotkeras.py
@@ -47,7 +47,7 @@ def predict_class(sentence, model):
     # filter out predictions below a threshold
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.65
+    ERROR_THRESHOLD = 0.75
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
