e0bdb978bd0e48d3379f35bd5305795012a64df2
rushna nizami <rushnaulaziz@gmail.com>
2021-03-04 10:29:44 -0500

    stop words removed from intents.json

--- chatbotkeras.py
+++ chatbotkeras.py
@@ -47,16 +47,16 @@ def predict_class(sentence, model):
     # filter out predictions below a threshold
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.85
+    ERROR_THRESHOLD = 0.65
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
+    print(classes)
     if results:
         for r in results:
             return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     else:
-        print(classes)
         return_list.append({"intent": classes[-1], "probability": "0"})
     return return_list
 
